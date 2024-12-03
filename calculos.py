755698826676c3eaa684d13378fcf785e2da0ee8
Mateo2alejo <alejandroviracocha844@gmail.com>
2024-12-02 22:24:06 -0500

    Update calculos.py

--- calculos.py
+++ calculos.py
@@ -42,3 +42,26 @@ def calcularCuota(monto, interesAnual, numeroMeses):
     interesMensual = (interesAnual/12)/100  
     cuotaMensual = (monto*interesMensual)/(1-(1+interesMensual)**(-numeroMeses))
     return cuotaMensual
+
+def calcularEdad(añonacimiento):
+    añoactual = 2024  
+    if añonacimiento > añoactual:
+        return -1
+    elif añonacimiento < 0:
+        return -1
+    else:
+        return añoactual - añonacimiento
+
+resultado1 = calcularEdad(2050)
+if resultado1 == -1:
+    print("Año nacimiento: 2050 => Usted aun no nace, o nació antes de cristo")
+
+resultado2 = calcularEdad(-10)
+if resultado2 == -1:
+    print("Año nacimiento: -10 => Usted aun no nace, o nació antes de cristo")
+
+resultado3 = calcularEdad(2024)
+print(f"Año nacimiento: 2024 => Este año cumple {resultado3} años")
+
+resultado4 = calcularEdad(2013)
+print(f"Año nacimiento: 2013 => Este año cumple {resultado4} años")
